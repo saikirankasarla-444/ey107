d89ec54712ffbc31f1b85441b2437115f9facd44
saikirankasarla-444 <saikiranvvvv@gmail.com>
2026-01-07 14:23:53 +0530

    first commit

---
+++ src/main/java/com/ey/Ey107Application.java
@@ -0,0 +1,13 @@
+package com.ey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ey107Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ey107Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/ey/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.ey.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@GetMapping
+	public String home() {
+		return "Hello World";
+	}
+}
