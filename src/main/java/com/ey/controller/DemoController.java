40cf9f034ec29f1a0fd183892710863cfb3eac9f
saikirankasarla-444 <saikiranvvvv@gmail.com>
2026-01-07 14:30:23 +0530

    second commit

--- src/main/java/com/ey/controller/DemoController.java
+++ src/main/java/com/ey/controller/DemoController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 	@GetMapping
 	public String home() {
-		return "Hello World";
+		return "Hello World evryone";
 	}
 }
